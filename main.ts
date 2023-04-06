a5a279cba667b63f07131baee413a8ef1d2d8fc5
400764 <115142394+400764@users.noreply.github.com>
2023-04-06 16:29:05 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,11 +7,11 @@ input.onGesture(Gesture.Shake, function () {
 input.onLogoEvent(TouchButtonEvent.Touched, function () {
     if (開始 == 0) {
         開始 += 1
-        music.playMelody("C D E F G A B C5 ", 120)
+        music.playMelody("C D E F G A B C5 ", 300)
     } else {
         開始 += -1
         basic.clearScreen()
-        music.playMelody("C5 B A G F E D C ", 120)
+        music.playMelody("C5 B A G F E D C ", 300)
     }
 })
 let 開始 = 0
